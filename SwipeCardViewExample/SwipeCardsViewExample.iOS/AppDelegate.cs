22e4d234e4c92bec3ea2d26b162673f9d67c3200
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-12-23 22:12:39 -0300

    iOS project added in SwipeCardViewExample.

---
+++ SwipeCardViewExample/SwipeCardsViewExample.iOS/AppDelegate.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using SwipeCardViewExample.Core;
+using SwipeCardViewExample.Forms.UI;
+using UIKit;
+
+namespace SwipeCardsViewExample.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp>
+    {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
