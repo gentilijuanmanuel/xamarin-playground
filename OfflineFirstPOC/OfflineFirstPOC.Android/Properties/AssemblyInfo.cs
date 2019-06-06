1237c4420da4ed4f95f9a8002c62690ed17ca471
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-06-06 10:58:17 -0300

    Implemented handle connectivity change with Xamarin.Essentials.

--- OfflineFirstPOC/OfflineFirstPOC.Android/Properties/AssemblyInfo.cs
+++ OfflineFirstPOC/OfflineFirstPOC.Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
@@ -15,6 +14,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
 
 // Version information for an assembly consists of the following four values:
 //
--- OfflineFirstPOC/OfflineFirstPOC.Core/App.xaml.cs
+++ OfflineFirstPOC/OfflineFirstPOC.Core/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using OfflineFirstPOC.Core.ViewModels;
 using OfflineFirstPOC.Core.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace OfflineFirstPOC.Core
@@ -10,7 +11,22 @@ namespace OfflineFirstPOC.Core
         {
             InitializeComponent();
 
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+
             MainPage = new NavigationPage(new MainPage(new MainViewModel()));
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if(e.NetworkAccess == NetworkAccess.Internet)
+            {
+                RunSync(); 
+            }
+        }
+
+        private void RunSync()
+        {
+            System.Console.WriteLine("hola!!!"); 
         }
     }
 }
