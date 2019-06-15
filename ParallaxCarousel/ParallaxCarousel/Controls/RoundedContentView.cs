17183381d0163d933a2c7b27b4918c713196eb8a
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-06-15 18:21:43 -0300

    Deleted useless comments in RoundedContentView (ParallaxCarousel).

--- ParallaxCarousel/ParallaxCarousel/Controls/RoundedContentView.cs
+++ ParallaxCarousel/ParallaxCarousel/Controls/RoundedContentView.cs
@@ -2,7 +2,7 @@
 
 namespace ParallaxCarousel.Controls
 {
-    public class RoundedContentView : ContentView
+    public class RoundedContentView : Frame
     {
         public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(
             nameof(HasShadow),
@@ -94,58 +94,6 @@ namespace ParallaxCarousel.Controls
             get => (Color)GetValue(GradientEndColorProperty);
             set => SetValue(GradientEndColorProperty, value);
         }
-
-        //public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(RoundedContentView), default(bool));
-        //public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(RoundedContentView), default(float));
-
-        //public static readonly BindableProperty ShadowElevationProperty = BindableProperty.Create(nameof(ShadowElevation), typeof(float), typeof(RoundedContentView),  40f);
-        //public static readonly BindableProperty ShadowTranslationZProperty = BindableProperty.Create(nameof(ShadowTranslationZ), typeof(float), typeof(RoundedContentView),  0f);
-
-        //public static readonly BindableProperty HasBackgroundGradientProperty = BindableProperty.Create(nameof(HasBackgroundGradient), typeof(bool), typeof(RoundedContentView), default(bool));
-        //public static readonly BindableProperty GradientStartColorProperty = BindableProperty.Create(nameof(GradientStartColor), typeof(Color), typeof(RoundedContentView), default(Color));
-        //public static readonly BindableProperty GradientEndColorProperty = BindableProperty.Create(nameof(GradientEndColor), typeof(Color), typeof(RoundedContentView), default(Color));
-
-        //public bool HasBackgroundGradient
-        //{
-        //    get { return (bool)GetValue(HasBackgroundGradientProperty); }
-        //    set { SetValue(HasBackgroundGradientProperty, value); }
-        //}
-
-        //public Color GradientStartColor
-        //{
-        //    get { return (Color)GetValue(GradientStartColorProperty); }
-        //    set { SetValue(GradientStartColorProperty, value); }
-        //}
-
-        //public Color GradientEndColor
-        //{
-        //    get { return (Color)GetValue(GradientEndColorProperty); }
-        //    set { SetValue(GradientEndColorProperty, value); }
-        //}
-
-        //public bool HasShadow
-        //{
-        //    get { return (bool)GetValue(HasShadowProperty); }
-        //    set { SetValue(HasShadowProperty, value); }
-        //}
-
-        //public float ShadowElevation
-        //{
-        //    get { return (float)GetValue(ShadowElevationProperty); }
-        //    set { SetValue(ShadowElevationProperty, value); }
-        //}
-
-        //public float ShadowTranslationZ
-        //{
-        //    get { return (float)GetValue(ShadowTranslationZProperty); }
-        //    set { SetValue(ShadowTranslationZProperty, value); }
-        //}
-
-        //public float CornerRadius
-        //{
-        //    get { return (float)GetValue(CornerRadiusProperty); }
-        //    set { SetValue(CornerRadiusProperty, value); }
-        //}
     }
 }
 
